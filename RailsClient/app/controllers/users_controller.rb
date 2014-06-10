4c6b002ca7c0d6f142e71dad0da578c663efeb7a
tesla lee <leechee89@gmail.com>
2014-06-10 13:56:33 +0800

    users path

--- RailsClient/app/controllers/users_controller.rb
+++ RailsClient/app/controllers/users_controller.rb
@@ -1,17 +1,46 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
   end
 
   def create
+    @user = User.new(user_params)
 
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'Location was successfully created.' }
+        format.json { render :show, status: :created, user: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
   end
 
   def show
-    @user=User.find(params[:id])
+=begin
     unless @user ==current_user
       redirect_to :back, :alert => "Access denied"
     end
+=end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email,:name,:tel,:password,:password_confirmation)
   end
 end
--- RailsClient/config/routes.rb
+++ RailsClient/config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :fortlifts
 
-  #resources :users
+  resources :users
 
   root :to => "welcome#index"
 
