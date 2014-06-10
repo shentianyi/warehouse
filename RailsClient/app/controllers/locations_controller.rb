dfd4aa72c8a4f1a404265bdccbf1dab722929b21
tesla lee <leechee89@gmail.com>
2014-06-10 12:30:28 +0800

    location view

--- RailsClient/app/controllers/locations_controller.rb
+++ RailsClient/app/controllers/locations_controller.rb
@@ -69,6 +69,7 @@ class LocationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
-      params[:location]
+      #params[:location]
+      params.require(:location).permit(:name)
     end
 end
