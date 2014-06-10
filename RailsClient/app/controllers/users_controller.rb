ea9cd3d8c4a7fccbeccec59b2e9367d4f38db077
tesla lee <leechee89@gmail.com>
2014-06-10 14:22:24 +0800

    user destroy

--- RailsClient/app/controllers/users_controller.rb
+++ RailsClient/app/controllers/users_controller.rb
@@ -27,11 +27,14 @@ class UsersController < ApplicationController
   end
 
   def show
-=begin
-    unless @user ==current_user
-      redirect_to :back, :alert => "Access denied"
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
     end
-=end
   end
 
   private
