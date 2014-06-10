f107d5c054fc4ebaba341b40b1e76406ee74e767
tesla lee <leechee89@gmail.com>
2014-06-10 15:12:49 +0800

    users

--- RailsClient/app/models/user.rb
+++ RailsClient/app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   include Extensions::UUID
 
+  belongs_to :location
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
