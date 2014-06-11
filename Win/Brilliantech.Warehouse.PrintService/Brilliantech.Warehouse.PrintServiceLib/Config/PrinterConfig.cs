14de29cb861c550e53d09e4cfa5b44516212c118
Charlot <7wangsong@gmail.com>
2014-06-11 15:07:08 +0800

    init

---
+++ Win/Brilliantech.Warehouse.PrintService/Brilliantech.Warehouse.PrintServiceLib/Config/PrinterConfig.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using Brilliantech.Warehouse.PrintServiceLib.Model;
+using Brilliantech.Framwork.Utils.ConfigUtil;
+ 
+
+namespace Brilliantech.Warehouse.PrintServiceHost.Config
+{
+    public class PrinterConfig
+    {
+        private static ConfigUtil printerConfig;
+        private static List<Printer> printers;
+
+        static PrinterConfig()
+        {
+            try
+            {
+                printerConfig = new ConfigUtil("Ini/printers.ini");
+                List<string> printerIds = printerConfig.Notes();
+                printers = new List<Printer>();
+                foreach (string id in printerIds)
+                {
+                    printers.Add(new Printer()
+                    {
+                        Id = id,
+                        Output = printerConfig.Get("Output", id),
+                        Template = printerConfig.Get("Template", id),
+                        Name = printerConfig.Get("Name", id),
+                        Type = int.Parse(printerConfig.Get("Type", id)),
+                        Copy = int.Parse(printerConfig.Get("Copy", id))
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+        public static List<Printer> Printers
+        {
+            get { return PrinterConfig.printers; }
+            set { PrinterConfig.printers = value; }
+        }
+
+        public static void Save(Printer printer)
+        {
+            printerConfig.Set("Name", printer.Name, printer.Id);
+            printerConfig.Set("Type", printer.Type, printer.Id);
+            printerConfig.Set("Copy", printer.Copy, printer.Id);
+            printerConfig.Save();
+        }
+    }
+}
---
+++ Win/Brilliantech.Warehouse.PrintService/Brilliantech.Warehouse.PrintServiceLib/Model/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Brilliantech.Warehouse.PrintServiceLib.Model
+{
+    public class Printer
+    {
+        public string Id { get;set; }
+        public string Output { get; set; }
+        public string Template { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public int Copy { get; set; }
+    }
+}
