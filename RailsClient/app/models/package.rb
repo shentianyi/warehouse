7ba7ee399e1fa333c5136436c39aa0f6f725803c
tesla lee <leechee89@gmail.com>
2014-06-11 16:24:54 +0800

    fix

--- RailsClient/app/models/package.rb
+++ RailsClient/app/models/package.rb
@@ -2,5 +2,17 @@ class Package < ActiveRecord::Base
   include Extensions::UUID
 
   belongs_to :fortlift
+  has_one :package_position
   has_many :state_logs, as: :stateable
+
+  #here is code for Leoni
+  after_save :set_position
+
+  private
+  def set_position
+    #if partnum changed, reset package position
+    if self.partnum_changed?
+
+    end
+  end
 end
