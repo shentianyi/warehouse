838fa1be4276946ee5a30823cd88a663bb1424c0
liqites <leechee89@gmail.com>
2014-06-13 00:02:04 +0800

    users sessions controller

--- RailsClient/app/controllers/users/sessions_controller.rb
+++ RailsClient/app/controllers/users/sessions_controller.rb
@@ -7,14 +7,14 @@ class Users::SessionsController<Devise::SessionsController
     self.resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    #yield resource if block_given?
+    #respond_with resource, location: after_sign_in_path_for(resource)
+    render :json=>{result: true}
   end
 
   def destroy
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
     set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
-    yield if block_given?
-    #respond_to_on_destroy
+    render :json => {result: true}
   end
 end
