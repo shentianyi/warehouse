778b5d6e950423381df2764d960ae461b360ef38
Charlot <7wangsong@gmail.com>
2014-06-16 02:13:33 +0800

    add delivery print method

--- RailsClient/app/service/printer/p001.rb
+++ RailsClient/app/service/printer/p001.rb
@@ -1,7 +1,7 @@
 module Printer
   class P001<Base
-    Head=[:id, :whouse, :delivery_date, :user]
-    Body=[:package_id, :part_id, :quantity, :w_date, :receive_position]
+    HEAD=[:id, :whouse, :delivery_date, :user]
+    BODY=[:package_id, :part_id, :quantity, :w_date, :receive_position]
 
     def initialize(id=nil)
       self.data_set =[]
@@ -12,14 +12,14 @@ module Printer
       f=Forklift.find(self.id)
       head={id: f.id, whouse: f.whouse_id, delivery_date: f.created_at, user: f.user_id}
       heads=[]
-      Head.each do |k|
+      HEAD.each do |k|
         heads<<{Key: k, Value: head[k]}
       end
       packages=f.packages
       packages.each do |p|
         body={package_id: p.id, part_id: p.part_id, quantity: p.quantity, w_date: p.in_date, receive_position: 'POSITION'}
         bodies=[]
-        Body.each do |k|
+        BODY.each do |k|
           bodies<<{Key: k, Value: body[k]}
         end
         self.data_set <<(heads+bodies)
