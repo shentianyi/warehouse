87306ef5efbb13617d36df3e9080030c5fa8287e
Charlot <7wangsong@gmail.com>
2014-06-15 22:16:39 +0800

    add extention and generate id

--- RailsClient/app/service/printer/p001.rb
+++ RailsClient/app/service/printer/p001.rb
@@ -1,14 +1,21 @@
 module Printer
   class P001<Base
+    Head=[:id,:whouse,:delivery_date,:user]
+    Body=[:package_id,:part_id,:quantity,:w_date,:receive_position]
     def initialize(id=nil)
-      self.data_set=[]
-      for i in 0...10
-        data=[]
-        [:Body, :Head].each { |key|
-          data<<{Key: key, Value: 'Value'+i.to_s}
-        }
-        self.data_set<<data
-      end
+      #self.data_set=[]
+      #for i in 0...10
+      #  data=[]
+      #  [:Body, :Head].each { |key|
+      #    data<<{Key: key, Value: 'Value'+i.to_s}
+      #  }
+      #  self.data_set<<data
+      #end
+
+    end
+
+    def generate_head
+
     end
   end
 end
