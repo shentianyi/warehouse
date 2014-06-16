211c18b52b582ca49430f8c0cc1f1facd24ddfa4
Charlot <7wangsong@gmail.com>
2014-06-16 13:59:01 +0800

    add setup project

--- Win/Brilliantech.Warehouse.PrintService/Brilliantech.Warehouse.PrintServiceHost/App.xaml.cs
+++ Win/Brilliantech.Warehouse.PrintService/Brilliantech.Warehouse.PrintServiceHost/App.xaml.cs
@@ -4,13 +4,31 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using Microsoft.Shell;
 
 namespace Brilliantech.Warehouse.PrintServiceHost
 {
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        private const string _uniq = "Brilliantech_Warehouse_Print_Service";
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(_uniq))
+            {
+                App app = new App();
+                app.InitializeComponent();
+                app.Run();
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            return true;
+        }
     }
 }
