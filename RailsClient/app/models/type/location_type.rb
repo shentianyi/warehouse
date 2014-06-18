c8b4666881c1c7f0d67fa92cb6a561ebd34f8be8
tesla lee <leechee89@gmail.com>
2014-06-18 18:42:15 +0800

    destination location

--- RailsClient/app/models/type/location_type.rb
+++ RailsClient/app/models/type/location_type.rb
@@ -1,3 +1,27 @@
 class LocationType
   BASE = 0
+  NORMAL = 1
+  DESTINATION = 2
+
+  def self.menu
+    data = []
+    self.constants.each do |c|
+      v = self.const_get(c.to_s)
+      data << [self.display(v),v]
+    end
+    data
+  end
+
+  def self.display type
+    case type
+      when BASE
+        '基礎'
+      when NORMAL
+        '普通'
+      when DESTINATION
+        '默認目的地'
+      else
+        '未知'
+    end
+  end
 end
