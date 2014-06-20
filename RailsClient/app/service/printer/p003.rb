12b3562d2fb3a520a4fc71528c780ba02da46a37
Charlot <7wangsong@gmail.com>
2014-06-20 14:57:54 +0800

    fix print

--- RailsClient/app/service/printer/p003.rb
+++ RailsClient/app/service/printer/p003.rb
@@ -5,7 +5,7 @@ module Printer
 
     def generate_data
       d=Delivery.find(self.id)
-      head={id: d.id, receive_addr:d.destination.address,user:d.receiver_id,receive_date:d.received_date.strftime('%Y.%m.%d')}
+      head={id: d.id, receive_addr:'Default',user:d.receiver_id,receive_date:d.received_date.strftime('%Y.%m.%d')}
       heads=[]
       HEAD.each do |k|
         heads<<{Key: k, Value: head[k]}
