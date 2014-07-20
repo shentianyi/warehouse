6487a754270296679f03ca89773eba4342a1c741
Charlot <7wangsong@gmail.com>
2014-07-20 23:37:01 +0800

    update print package list method

--- RailsClient/app/service/printer/p001.rb
+++ RailsClient/app/service/printer/p001.rb
@@ -1,3 +1,4 @@
+# print package list
 module Printer
   class P001<Base
     HEAD=[:id, :whouse, :delivery_date, :user]
@@ -6,7 +7,6 @@ module Printer
 
     def generate_data
       f=Forklift.find_by_id(self.id)
-
       head={id: f.id, whouse: f.whouse.name, delivery_date: f.created_at.strftime('%Y.%m.%d'), user: f.stocker_id}
       heads=[]
       HEAD.each do |k|
@@ -14,7 +14,11 @@ module Printer
       end
       packages=f.packages
       packages.each do |p|
-        body={package_id: p.id, part_id: p.part_id, quantity: p.quantity_str, w_date: p.check_in_time, receive_position: p.get_position}
+        body={package_id: p.id,
+              part_id: PackageLabelRegex.part_prefix_string+ p.part_id,
+              quantity: PackageLabelRegex.quantity_prefix_string+p.quantity_str,
+              w_date: PackageLabelRegex.date_prefix_string+p.check_in_time,
+              receive_position: p.get_position}
         bodies=[]
         BODY.each do |k|
           bodies<<{Key: k, Value: body[k]}
