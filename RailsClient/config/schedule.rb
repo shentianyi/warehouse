e679a6146bca470c9083ba8f9cf92dfe9bfb22a1
root <root@AY1306061001362237abZ.(none)>
2014-07-22 18:19:47 +0800

    fix

--- RailsClient/config/schedule.rb
+++ RailsClient/config/schedule.rb
@@ -20,6 +20,6 @@ set :output, {:error => 'log/cron_error_log.log', :standard => 'log/cron_log.log
 
 # Learn more: http://github.com/javan/whenever
 
-every 5.minutes do
+every 2.minutes do
   rake 'sync:execute'
 end
