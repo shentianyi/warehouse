547951d10d69134e4456454c737484c1d5c8f20f
tesla lee <leechee89@gmail.com>
2014-07-24 16:23:57 +0800

    order api

--- RailsClient/app/service/order_service.rb
+++ RailsClient/app/service/order_service.rb
@@ -5,15 +5,15 @@ class OrderService
   #params
   # @user_id:current_user.id,
   # @days: integer
-  # @state: integer,0
+  # @state: false
   #=============
   def self.get_orders_by_days days,user_id = nil,handled = false
+    start_time = days.days.ago.at_beginning_of_day.utc
+    end_time = Time.now.at_end_of_day.utc
     if user_id.nil?
-      start_time = days.days.ago.at_beginning_of_day.utc
-      end_time = Time.now.at_end_of_day.utc
       Order.where(created_at:(start_time..end_time),handled:handled).all.order(created_at: :desc)
     else
-
+      Order.where(created_at:(start_time..end_time),user_id:user_id,handled:handled).all.order(created_at: :desc)
     end
   end
 
@@ -34,8 +34,8 @@ class OrderService
   #=============
   def self.history_orders_by_time args
     unless args[:start_time].class.name === "Time"
-      args[:start_time] = Time.parse(args[:start_time])
-      args[:end_time] = Time.parse(args[:end_time])
+      args[:start_time] = Time.parse(args[:start_time]).at_beginning_of_day
+      args[:end_time] = Time.parse(args[:end_time]).at_end_of_day
     end
 
     Order.where(user_id:args[:user_id],created_at:(args[:start_time]..args[:end_time])).all.order(created_at: :desc)
