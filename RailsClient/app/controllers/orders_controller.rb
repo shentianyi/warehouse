436decbcd1357ccdc85881b2c4bf644ba3f6f231
tesla lee <leechee89@gmail.com>
2014-07-25 15:52:07 +0800

    fix

--- RailsClient/app/controllers/orders_controller.rb
+++ RailsClient/app/controllers/orders_controller.rb
@@ -68,7 +68,7 @@ class OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+      @order = Order.unscoped.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
