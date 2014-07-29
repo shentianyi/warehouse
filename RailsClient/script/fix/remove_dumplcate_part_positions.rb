c7eb8d90322edafd8b7c8c95cc80c98c7cd4c803
tesla lee <leechee89@gmail.com>
2014-07-29 12:10:55 +0800

    fix remove dumpilcate

--- RailsClient/script/fix/remove_dumplcate_part_positions.rb
+++ RailsClient/script/fix/remove_dumplcate_part_positions.rb
@@ -2,7 +2,12 @@ grouped = PartPosition.all.group_by{|pp|
 	[pp.part_id,pp.position_id]
 }
 
+i = 0
 grouped.values.each do |duplicates|
 	first_one = duplicates.shift
-	duplicates.each{|double| double.destroy}
-end
+	duplicates.each{|double|
+    i++
+    double.destroy
+  }
+end 
+puts '删除'+i.to_s+'条冗余数据'
