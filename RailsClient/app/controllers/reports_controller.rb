8ceef7a30675d92f5ec70562a3d47696547e9c4b
liqites <leechee89@gmail.com>
2014-08-04 17:14:18 +0800

    report

--- RailsClient/app/controllers/reports_controller.rb
+++ RailsClient/app/controllers/reports_controller.rb
@@ -1,12 +1,13 @@
+# encoding: utf-8
 class ReportsController < ApplicationController
-	def entry_report
+  def entry_report
+    @location_id = params[:location_id].nil? ? current_user.location_id : params[:location_id]
+    @received_date_start = params[:received_date_start].nil? ? 1.day.ago.strftime("%Y-%m-%d 7:00:00") : params[:received_date_start]
+    @received_date_end = params[:received_date_end].nil? ? Time.now.strftime("%Y-%m-%d 7:00:00") : params[:received_date_end]
+    time_range = Time.parse(@received_date_start).utc..Time.parse(@received_date_end).utc
 
-	end
-
-	private
-
-	def set_report_parameter
-		@location = p[:location]
-		
-	end
+    #add state
+    @packages = Delivery.includes(forklifts: :packages).where("destination_id"=>@location_id,"received_date" => time_range).paginate(:page=>params[:page])
+    #render :entry_report
+  end
 end
