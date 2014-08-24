f4815ff1edb283fc581f4e30bcb50e1c4cc41597
liqites <leechee89@gmail.com>
2014-08-24 23:42:25 +0800

    led_state

--- RailsClient/app/models/led_state.rb
+++ RailsClient/app/models/led_state.rb
@@ -1,3 +1,11 @@
 class LedState < ActiveRecord::Base
   include Extensions::UUID
+
+  validate :validate_create_update
+
+  private
+  def validate_create_update
+    errors.add(:name, "不能重复") if LedState.where(:name => self.name).first if new_record?
+    errors.add(:name, "不能重复") if LedState.where(:name => self.name).where('id<>?', self.id).first unless new_record?
+  end
 end
