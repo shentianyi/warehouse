513e7b2f2756d00f1e8c06e319960d05be0c8f47
tesla lee <leechee89@gmail.com>
2014-08-25 13:02:44 +0800

    fix

--- RailsClient/app/api/v1/led_api.rb
+++ RailsClient/app/api/v1/led_api.rb
@@ -20,16 +20,16 @@ module V1
     get :position_state do
       pp = OrderItemService.verify_department(params[:whouse],params[:part_id])
       if pp.nil?
-        {result:0,content:'库位不存在'}
+        return {result:0,content:'库位或零件不存在'}
       end
       led = Led.find_by_position(pp.position.detail)
       if led.nil?
-        {result:0,content:'LED灯未找到'}
+        return {result:0,content:'LED灯未找到'}
       end
 
       ls = LedState.find_by_state(led.current_state)
       if ls.nil?
-        {result:0,content:'LED灯状态错误,请重置'}
+        return {result:0,content:'LED灯状态错误,请重置'}
       end
 
       {result:1,content:{position:pp.position.detail,state:led.current_state}}
