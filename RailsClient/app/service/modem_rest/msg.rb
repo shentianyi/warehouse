eac84a9ef5b32e3ddfe7cf0d849182aae95062cc
Charlot <7wangsong@gmail.com>
2014-08-27 00:06:45 +0800

    rm useless

--- RailsClient/app/service/modem_rest/msg.rb
+++ RailsClient/app/service/modem_rest/msg.rb
@@ -28,19 +28,27 @@ module ModemRest
     end
 
     def get_change_led_state_url
-      "http://#{self.ip}:#{SysConfigCache.led_service_port_value}/#{SysConfigCache.led_send_msg_action_value}/#{get_change_led_state_msg}"
+      "http://#{self.ip}:#{SysConfigCache.led_service_port_value}/#{SysConfigCache.led_send_msg_action_value}#{get_change_led_state_msg}"
     end
 
     def get_change_led_state_msg
-      _msg=self.msg.split(' ').map { |m| m.to_i.to_s(16).scan(/.{1,2}/).map { |mm| mm.rjust(2, '0') } }.join(' ')
-      _led_id= self.led_id.to_i.to_s(16).rjust(4, '0').scan(/.{2}/).join(' ')
-      URI::escape("#{_msg} #{_led_id}")
+     #  _msg=self.msg.split(' ').map { |m| m.to_i.to_s(16).scan(/.{1,2}/).map { |mm| mm.rjust(2, '0') } }.join(' ')
+     #_led_id= self.led_id.to_i.to_s(16).rjust(4, '0').scan(/.{2}/).join(' ')
+     # msg= "#{_msg} #{_led_id}"
+      #//msg=msg.split(' ').collect{|m| m='0x'+m}.join(' ')
+      #puts msg
+
+      #self.led_id=10
+#      if self.led_id.to_i>255
+        m="#{self.msg}  #{self.led_id}"
+ #     else
+  #      m="#{self.msg} 0 #{self.led_id}"
+   #   end
+      puts m
+      URI::escape(m)
     end
 
     def get_resource(url)
-      puts 'url----------------------'
-      puts url
-
       RestClient::Resource.new(url,
                                timeout: 100,
                       open_timeout: 100,
