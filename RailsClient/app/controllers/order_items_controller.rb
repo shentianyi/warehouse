fc3d2841a33df2ade3863a9b5376016fc4ca7ce4
Tesla <leechee89@gmail.com>
2014-09-23 16:34:45 +0800

    fix checkbox

--- RailsClient/app/controllers/order_items_controller.rb
+++ RailsClient/app/controllers/order_items_controller.rb
@@ -69,6 +69,7 @@ class OrderItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_item_params
-      params[:order_item]
+      #params[:order_item]
+      params.require(:order_item).permit(:is_finished,:out_of_stock)
     end
 end
