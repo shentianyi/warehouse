7e5570c0f5644d31511718080ed6c305076ea2fe
liqites <leechee89@gmail.com>
2014-11-04 16:30:50 +0800

    movable

--- RailsClient/app/models/base/movable.rb
+++ RailsClient/app/models/base/movable.rb
@@ -1,20 +1,22 @@
 module Movable
   #include this module to your Model,make sure you have column below
-  attr_accessor :current_position_id,:sender_id,:receiver_id,:delivery_date,:received_date,:state,:locked,:destination_id
+  attr_accessor :current_position_id,:destination_id,:sender_id,:receiver_id,:delivery_date,:received_date
 
-  def setout(destination_id)
+  def set_out(destination_id,sender_id = nil)
 
+    self.destination_id = destination_id
+    if sender_id
+      self.sender_id = sender_id
+    end
+    self.delivery_date = Time.now
   end
 
-  def before_setout
+  def arrived(receiver_id=nil)
 
-  end
-
-  def arrived
-
-  end
-
-  def before_arrived
-
+    self.current_position_id = self.destination_id
+    if receiver_id
+      self.receiver_id = receiver_id
+    end
+    self.received_date = Time.now
   end
 end
