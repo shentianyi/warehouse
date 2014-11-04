c8f2f6aa0ac40b3a3c39fb4795a883a96b372085
liqites <leechee89@gmail.com>
2014-11-04 17:45:58 +0800

    schema

--- RailsClient/db/schema.rb
+++ RailsClient/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103105230) do
+ActiveRecord::Schema.define(version: 20141104092557) do
 
   create_table "api_logs", force: true do |t|
     t.string   "user_id"
