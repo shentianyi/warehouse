21ba704ff6c12f9324d62785dad25b3253391682
liqites <tesla@hdp-dn2.(none)>
2014-11-04 12:16:42 +0800

    movable

--- RailsClient/app/models/base/movable.rb
+++ RailsClient/app/models/base/movable.rb
@@ -1,6 +1,20 @@
 module Movable
   #include this module to your Model,make sure you have column below
-  attr_accessor :current_position_id,:sender_id,:receiver_id,:delivery_date,:received_date,:state,:locked
+  attr_accessor :current_position_id,:sender_id,:receiver_id,:delivery_date,:received_date,:state,:locked,:destination_id
 
-  
+  def setout(destination_id)
+
+  end
+
+  def before_setout
+
+  end
+
+  def arrived
+
+  end
+
+  def before_arrived
+
+  end
 end
