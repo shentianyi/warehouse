71da62ca4abacbb540d74096a562cfb4179a930b
Tesla <leechee89@gmail.com>
2014-11-05 17:27:48 +0800

    rspec

--- RailsClient/spec/api/delivery_api_test.rb
+++ RailsClient/spec/api/delivery_api_test.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 describe "DeliveryAPIV1" do
-  describe "#list" do
-    context "get all deliveries" do
-      it "show return 10 deliveries" do
-        get '/api/v1/deliveries/list/', {:delivery_date => "2014-09-30"}
-        #expect(JSON.parse(response.body)).to be_success
-        should respond_with 200
-      end
-    end
+  login_admin
+
+  it "show return 10 deliveries" do
+    get '/api/v1/deliveries/list/', {:delivery_date => "2014-09-30"}
+    expect(JSON.parse(response.body)).to eq(1)
+    puts JSON.parse(response)
   end
 end
