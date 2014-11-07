691f94480d7aac9c0c2125fe0d82644850f87375
Tesla <leechee89@gmail.com>
2014-11-07 17:24:20 +0800

    package api rspec test

--- RailsClient/spec/api/package_api_spec.rb
+++ RailsClient/spec/api/package_api_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 describe V1::PackageAPI do
-  before(:each) do
+
+  before(:all) do
     @user = create(:user)
     login(@user)
+
+    @part = create(:part)
   end
 
-  after(:each) do
+  after(:all) do
     logout
   end
 
@@ -15,8 +18,15 @@ describe V1::PackageAPI do
     it "should get packages" do
       get "/api/v1/packages/binds"
 
-      puts(JSON.parse(response.body))
       expect(response.status).to eq(200)
+    end
+  end
+
+  describe "POST /api/v1/packages/validate" do
+    it "should get valid id" do
+      post "/api/v1/packages/validate",{id:"WI110"}
+      puts JSON.parse(response.body)
+      expect(JSON.parse(response.body)).to eq({"result" => 1,"content" => ""})
     end
   end
 end
