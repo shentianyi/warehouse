fde69d27b2b8fbb2926194fb8193b5f22801df1e
Charlot <7wangsong@gmail.com>
2014-11-21 14:01:31 +0800

    p003

--- RailsClient/app/presenters/delivery_presenter.rb
+++ RailsClient/app/presenters/delivery_presenter.rb
@@ -1,5 +1,5 @@
 class DeliveryPresenter<Presenter
-  Delegators=[:id,:container_id,:destinationable,:created_at,:state,:user_id,:source_location_id,:des_location_id,:remark]
+  Delegators=[:id, :container_id, :destinationable, :created_at, :state, :user_id, :source_location_id, :des_location_id, :remark]
   def_delegators :@delivery, *Delegators
 
   def initialize(delivery_lc)
@@ -9,7 +9,7 @@ class DeliveryPresenter<Presenter
 
   def delivery_date
     dispatch = @delivery.get_record('dispatch')
-    dispatch.nil? ? '' :dispatch.impl_time.localtime.strftime('%Y.%m.%d %H:%M')
+    dispatch.nil? ? '' : dispatch.impl_time.localtime.strftime('%Y.%m.%d %H:%M')
   end
 
   def received_date
@@ -17,9 +17,14 @@ class DeliveryPresenter<Presenter
     receive.nil? ? '' : receive.impl_time.localtime.strftime('%Y.%m.%d %H:%M')
   end
 
+  def received_user
+    receive = @delivery.get_record('receive')
+    receive.nil? ? '' : receive.impl_id
+  end
+
   def to_json
     {
-        id:self.id,
+        id: self.id,
         delivery_date: self.delivery_date,
         received_date: self.received_date,
         state: self.state,
@@ -27,7 +32,7 @@ class DeliveryPresenter<Presenter
         can_delete: false, #notice
         user_id: self.user_id,
         destination_id: self.des_location_id,
-        remark:self.remark
+        remark: self.remark
     }
   end
 end
