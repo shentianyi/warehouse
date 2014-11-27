e1e65babbab799f0719664b73edac1aa9412be69
Tesla <leechee89@gmail.com>
2014-11-27 11:38:19 +0800

    after this pull,please run rake db:migrate;rails runner script/fix/add_user_name_to_user.rb;

--- RailsClient/db/schema.rb
+++ RailsClient/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112052934) do
+ActiveRecord::Schema.define(version: 20141126082819) do
 
   create_table "api_logs", force: true do |t|
     t.string   "user_id"
