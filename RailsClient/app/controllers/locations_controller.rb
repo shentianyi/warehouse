a2523006ec843bf9ce27f0ea242ee28e7ba75972
Tesla <leechee89@gmail.com>
2014-11-28 14:23:18 +0800

    destinations page

--- RailsClient/app/controllers/locations_controller.rb
+++ RailsClient/app/controllers/locations_controller.rb
@@ -1,6 +1,6 @@
 class LocationsController < ApplicationController
   load_and_authorize_resource
-  before_action :set_location, only: [:show, :edit, :update, :destroy, :users, :whouses,:destinations,:add_destination,:remove_destination]
+  before_action :set_location, only: [:show, :edit, :update, :destroy, :users, :whouses,:destinations,:add_destination,:remove_destination,:set_default_destination]
 
   # GET /locations
   # GET /locations.json
@@ -46,22 +46,42 @@ class LocationsController < ApplicationController
 
   def add_destination
     if @location.destinations.include?(Location.find_by_id(location_params[:destination_id]))
-      render :destinations
-    elsif @location.location_destinations.build({destination_id:location_params[:destination_id]}).save
-      redirect_to destinations_location_path(@location), notice: '添加目的地成功'
+      redirect_to destinations_location_path(@location), notice: '目的地未找到'
+    end
+
+    if @location.location_destinations.count == 0
+      d = @location.location_destinations.build({destination_id:location_params[:destination_id],is_default:true})
     else
-      render :destinations
+      d = @location.location_destinations.build({destination_id:location_params[:destination_id]})
+    end
+
+    if d.save
+      redirect_to destinations_location_path(@location), notice: '添加成功'
+    else
+      redirect_to destinations_location_path(@location), notice: d.errors.full_messages.to_json
     end
   end
 
   def remove_destination
-      d = @location.location_destinations.where({destination_id:params[:destination_id]}).first
-      puts d.to_json
-      if d.destroy
-        render :destinations ,notice: '删除成功'
-      else
-        render :destinations
-      end
+    d = @location.location_destinations.where({destination_id:params[:destination_id]}).first
+    d.destroy
+    render :destinations
+    #render :json => d.errors.full_messages.to_json
+  end
+
+  def set_default_destination
+    unless @location.destinations.include?(Location.find_by_id(params[:destination_id]))
+      redirect_to destinations_location_path(@location), notice: '目的地未找到'
+    end
+
+    @location.location_destinations.where({is_default:true}).first.update({is_default:false})
+
+    d = @location.location_destinations.where({destination_id:params[:destination_id]}).first
+    if d.update({is_default:true})
+      redirect_to destinations_location_path(@location), notice: '设置成功'
+    else
+      redirect_to destinations_location_path(@location), notice: '设置失败'
+    end
   end
 
   # GET /locations/1/users
