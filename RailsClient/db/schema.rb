48d0c2ab2a22a2aaef9450242deeb6783217ed9f
Charlot <7wangsong@gmail.com>
2014-11-30 22:37:56 +0800

    storage search

--- RailsClient/db/schema.rb
+++ RailsClient/db/schema.rb
@@ -180,7 +180,6 @@ ActiveRecord::Schema.define(version: 20141128090900) do
     t.string   "user_id"
     t.string   "container_id"
     t.string   "remark"
-    t.integer  "type"
     t.boolean  "is_delete",            default: false
     t.boolean  "is_dirty",             default: true
     t.boolean  "is_new",               default: true
@@ -190,6 +189,7 @@ ActiveRecord::Schema.define(version: 20141128090900) do
     t.string   "destinationable_id"
     t.string   "destinationable_type"
     t.string   "ancestry"
+    t.integer  "type"
   end
 
   add_index "location_containers", ["ancestry"], name: "index_location_containers_on_ancestry", using: :btree
