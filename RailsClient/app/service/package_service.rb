08dee867c56dbbdee67037ef2a7cb07b01dee085
Tesla <leechee89@gmail.com>
2014-12-02 17:23:47 +0800

    fix

--- RailsClient/app/service/package_service.rb
+++ RailsClient/app/service/package_service.rb
@@ -2,16 +2,16 @@ class PackageService
   #=============
   #dispatch
   #=============
-    ActiveRecord::Base.transaction do
-    unless (m = lc.get_movable_service.dispatch(lc,destination,user)).result
+  def self.dispatch lc, destination, user
+    unless (m = lc.get_movable_service.dispatch(lc, destination, user)).result
       return m
     end
 
     return Message.new.set_true
   end
 
-  def self.receive lc,user
-    unless (m = lc.get_movable_service.receive(lc,user)).result
+  def self.receive lc, user
+    unless (m = lc.get_movable_service.receive(lc, user)).result
       return m
     end
 
@@ -63,9 +63,9 @@ class PackageService
   end
 
 
-#=============
-#update @package
-#=============
+  #=============
+  #update @package
+  #=============
   def self.update args
     msg = Message.new
     unless lc= LogisticsContainer.exists?(args[:id])
