c3fa7a0e1ec7764febc73a521daa8daa2e45a61b
Tesla <leechee89@gmail.com>
2014-12-10 16:44:55 +0800

    del

--- RailsClient/script/migrate/location_migrate.rb
+++
@@ -1,8 +0,0 @@
-Location.all.each do |l|
-  if l.destination_id && (l.location_destinations.where(destination_id:l.destination_id).count == 0)
-    ld = l.location_destinations.build({location_id:l.id,destination_id:l.destination_id,is_default:true})
-    ld.save
-    puts "-----------------"
-    puts ld.destination.name
-  end
-end
