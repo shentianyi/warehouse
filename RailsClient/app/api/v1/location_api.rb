7acc3a2b73dda19555b48a8b20b0f8d70fd6ba08
Tesla <leechee89@gmail.com>
2014-12-12 17:13:23 +0800

    fix

--- RailsClient/app/api/v1/location_api.rb
+++ RailsClient/app/api/v1/location_api.rb
@@ -25,5 +25,22 @@ module V1
       }
       data
     end
+
+    get :test do
+      msg = ApiMessage.new
+      model = params[:model].constantize
+      if params[:skip] && params[:skip].to_i == 1
+        puts "-----------#{model} skip_callbacks"
+        model.record_timestamps=false
+        model.skip_callback(:update, :before, :reset_dirty_flag)
+        model.skip_callback(:create,:before,:init_container_attr)
+      else
+        puts "-----------#{model} reset_callbacks"
+        model.record_timestamps=true
+        model.set_callback(:update, :before, :reset_dirty_flag)
+        model.set_callback(:create,:before,:init_container_attr)
+      end
+      msg
+    end
   end
 end
