c99fa2c8e00685151d30dcc64e5de1ffa02a0d24
Tesla <leechee89@gmail.com>
2014-12-29 21:22:06 +0800

    fix

--- RailsClient/config/schedule.rb
+++ RailsClient/config/schedule.rb
@@ -25,5 +25,17 @@ every 2.minute do
 end
 
 every 10.second do
-  rails runner 'script/test/test_delivery.rb'
+  sender = User.where({role_id:Role.sender}).first
+
+  package_id = "WI#{Time.now.to_milli}"
+
+  package = PackageService.create({id:package_id,part_id:Part.first,check_in_time:"12 15 13"},sender).object
+
+  forklift = ForkliftService.create({destinationable_id:Whouse.first.id},sender).object
+
+  forklift.add(package)
+
+  delivery = DeliveryService.create({},sender).object
+
+  delivery.add(forklift)
 end
