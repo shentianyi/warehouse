3814ca34d7d07b3a345ce86946f38157c21bc548
Tesla <leechee89@gmail.com>
2015-01-04 17:11:42 +0800

    fix

--- RailsClient/app/service/printer/p006.rb
+++ RailsClient/app/service/printer/p006.rb
@@ -1,12 +1,12 @@
 # print pick list
 module Printer
   class P006<Base
-    HEAD=[:id, :user_id, :created_at,:remark]
+    HEAD=[:id, :user_id, :created_at]
     BODY=[:part_id, :quantity, :box_quantity,:whouse_id, :is_emergency, :remark]
 
     def generate_data
       p=PickList.find_by_id(self.id)
-      head={id: p.id, user_id: p.user_id, created_at: p.created_at.localtime.strftime('%Y.%m.%d %H:%M:%S'),remark: p.remark}
+      head={id: p.id, user_id: p.user_id, created_at: p.created_at.localtime.strftime('%Y.%m.%d %H:%M:%S')}
       heads=[]
       HEAD.each do |k|
         heads<<{Key: k, Value: head[k]}
