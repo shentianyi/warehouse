94eb32429b64ebaf2cf537b30dd8766caae85166
Charlot <7wangsong@gmail.com>
2015-03-28 17:28:54 +0800

    con

--- RailsClient/app/api/v1/storage_api.rb
+++ RailsClient/app/api/v1/storage_api.rb
@@ -2,24 +2,6 @@ module V1
   class StorageAPI<Base
     namespace :storages do
 
-<<<<<<< HEAD
-      post :move_store do
-        ActiveRecord::Base.transaction do
-          if forklift=Forklift.exists?(params[:id])
-            if fsc=forklift.store_containers.where(source_location_id: current_user.location_id).first
-              fsc_destination=nil
-              fsc.descendants.joins(:package).each do |psc|
-                destination=psc.destinationable
-                psc.move_store(destination) if psc.can_move_store?(destination)
-                fsc_destination=destination
-              end
-              if fsc.can_move_store?(fsc_destination)
-                fsc.move_store(fsc_destination)
-                return {result: 1, content: '移库成功！'}
-              else
-                return {result: 0, content: '已经移库！'}
-              end
-=======
     post :move_store do
       ActiveRecord::Base.transaction do
         if forklift=Forklift.exists?(params[:id])
@@ -34,7 +16,6 @@ module V1
             if fsc.can_move_store?(fsc_destination)
               fsc.move_store(fsc_destination)
               return {result: 1, content: '移库成功！'}
->>>>>>> penglai-develop
             else
               return {result: 0, content: '还未入库！'}
             end
@@ -43,8 +24,8 @@ module V1
           end
         end
       end
-    end
-
+end
+end
     #以包装箱为单位进行移库
     post :move_storage do
 
