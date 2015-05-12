1c881238f3ff4a6197864a83e0df3e1df87bce77
Jet <ijetlee@gmail.com>
2015-05-12 15:24:01 +0800

    update response json format.

--- RailsClient/app/api/v3/location_api.rb
+++ RailsClient/app/api/v3/location_api.rb
@@ -2,7 +2,7 @@
 module V3
   class LocationApi < Grape::API
     rescue_from :all do |e|
-      error!("Error: #{e.class.name} : [#{e.message}]", 500)
+      {result: 0, content: "Error: #{e.class.name} : [#{e.message}]"}
     end
 
     helpers do
@@ -16,6 +16,7 @@ module V3
       get do
         # guard!
         locations = NLocation.all
+        {result: 1, content: locations}
       end
 
       params do
@@ -30,6 +31,7 @@ module V3
         parent = params[:parentId].present? ? NLocation.find_by!(parentId: params[:parentId]) : nil
         location = NLocation.create!(locationId: params[:locationId], name: params[:name], remark: params[:remark],
                                      status: params[:status], parent: parent)
+        {result: 1, content: location}
       end
     end
 
