e634040493898fbe033b43541de439c493436452
Jet <ijetlee@gmail.com>
2015-05-13 09:37:01 +0800

    fix bug: db column part_nr -> partNr.

--- RailsClient/app/api/v3/ware_house_api.rb
+++ RailsClient/app/api/v3/ware_house_api.rb
@@ -90,7 +90,7 @@ module V3
       # requires :fifo, type: String, desc: 'require fifo'
       requires :toWh, type: String, desc: 'require toWh(to warehouse, whId)'
       requires :toPosition, type: String, desc: 'require toPosition'
-      requires :type, type: String, desc: 'require move type'
+      # requires :type, type: String, desc: 'require move type'
       optional :fromWh, type: String, desc: 'require toWh(to warehouse, whId)'
       optional  :fromPosition, type: String, desc: 'require toPosition'
       optional :qty, type: Integer, desc: 'require qty(quantity)'
@@ -105,26 +105,84 @@ module V3
       type = MoveType.find_by!(typeId: 'MOVE')
       toWh = WareHouse.find_by!(whId: params[:toWh])
       validate_position(toWh, params[:toPosition])
+      data = {to_id: toWh.id, toPosition: params[:toPosition], type_id: type.id}
       if params[:uniqueId].present?
+        #Move(uniqueId,toWh,toPosition,type)
         # find from wh
         storage = NStorage.find_by!(uniqueId: params[:uniqueId])
+        # update parameters of movement creation
+        data.update({ from_id: storage.ware_house_id, fromPosition: storage.position,
+                      uniqueId: params[:uniqueId], qty: storage.qty, fifo: storage.fifo, partRr: storage.partNr})
         # create movement
-        data = {to_id: toWh.id, toPosition: params[:toPosition],
-                type_id: type.id, from_id: storage.ware_house_id, fromPosition: storage.position,
-                uniqueId: params[:uniqueId]}
         Movement.create!(data)
+
         # update storage
         storage.update!(ware_house_id: toWh.id, position: params[:toPosition])
       elsif params[:packageId].present?
+        # Move(packageId,partnr, quantity,toWh, toPosition,type)
         # find from wh
         storage = NStorage.find_by!(packageId: params[:packageId], partNr: params[:partNr])
         # validate package qty
         raise 'No enough qty in package' if params[:qty] > storage.qty
+        # update parameters of movement creation
+        data.update({ from_id: storage.ware_house_id, fromPosition: storage.position,
+                      packageId: params[:packageId], qty: params[:qty], fifo:storage.fifo, partNr: storage.partNr})
         # create movement
-        data = {qty: params[:qty], type_id: type.id, packageId: params[:packageId],
-                to_id: toWh.id, toPosition: params[:toPosition], partNr: params[:partNr],
-                from_id: storage.ware_house_id, fromPosition: storage.position}
         Movement.create!(data)
+        # adjust storage
+        ## adjust to storage
+        tostorage = NStorage.find_by(ware_house_id: toWh.id, partNr: params[:partNr], position: params[:toPosition])
+        if tostorage.present?
+          tostorage.update!(qty: tostorage.qty + params[:qty])
+        else
+          data = {partNr: params[:partNr], qty: params[:qty], fifo: storage.fifo, ware_house_id: toWh.id,
+                  position: params[:toPosition], packageId: params[:packageId]}
+          tostorage = NStorage.create!(data)
+        end
+        ## adjust src storage
+        if params[:qty] == storage.qty
+          storage.destroy!
+        else
+          storage.update!(qty: storage.qty - params[:qty])
+        end
+      elsif [:partNr, :qty, :fromWh, :fromPosition ].reduce(true){|seed, i| seed and params.include? i}
+        # Move(partNr, qty, fromWh,fromPosition,toWh,toPosition,type)
+        # Move(partNr, qty, fifo,fromWh,fromPosition,toWh,toPosition,type)
+        fromWh = WareHouse.find_by!(whId: params[:fromWh])
+        validate_position(fromWh, params[:fromPosition])
+        # find storage records
+        storages = NStorage.where(partNr: params[:partNr], ware_house_id: fromWh.id, position: params[:fromPosition])
+        # add fifo condition if fifo param exists
+        if params[:fifo]
+          fifo = validate_fifo_time(params[:fifo])
+          storage.where(fifo: fifo)
+        end
+        # order by fifo
+        storage.order(fifo: :asc)
+        # validate sum of storage qty is enough
+        raise 'No enough qty in source' if sumqty = storages.reduce(0){|seed, s| seed + s.qty} < params[:qty]
+        storages.reduce(params[:qty]) do |restqty, storage|
+          break if restqty <= 0
+          # update parameters of movement creation
+          data.update({ from_id: storage.ware_house_id, fromPosition: storage.position,
+                        fifo:storage.fifo, partNr: storage.partNr})
+          if restqty >= storage
+            # move all storage
+            storage.update!(ware_house_id: toWh.id, position: params[:toPosition])
+            data[:qty] = storage.qty
+            restqty = restqty - storage.qty
+          else
+            # adjust source storage
+            storage.update!(qty: storage.qty - restqty)
+            # create target storage
+            last = NStorage.create!(data)
+            data[:qty] = restqty
+            restqty = 0
+          end
+          # create movement
+          Movement.create!(data)
+          restqty
+        end
       end
 
     end
