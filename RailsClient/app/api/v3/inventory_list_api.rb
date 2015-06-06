85feef13b1bb624cba48f2465ff4019bc9527eb2
ryan715 <yxr.306789@gmail.com>
2015-06-06 18:27:41 +0800

    fix api of InventoryList Processing

--- RailsClient/app/api/v3/inventory_list_api.rb
+++ RailsClient/app/api/v3/inventory_list_api.rb
@@ -8,7 +8,11 @@ module V3
       desc 'get processing data'
       get :processing do
         inventory_lists = InventoryList.find_by_state(200)
-        { result: 1, content: inventory_lists }
+        if inventory_lists.blank?
+          { result: 1, content: "没有查询到相关记录." }
+        else 
+          { result: 1, content: inventory_lists }
+        end
       end
     end
   end
