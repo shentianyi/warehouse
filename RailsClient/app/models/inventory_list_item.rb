b144deacca706dd79708228121331e2c316ea1f1
Charlot <7wangsong@gmail.com>
2015-06-15 11:27:37 +0800

    update

--- RailsClient/app/models/inventory_list_item.rb
+++ RailsClient/app/models/inventory_list_item.rb
@@ -5,7 +5,7 @@ class InventoryListItem < ActiveRecord::Base
   #belongs_to :user
   belongs_to :inventory_list
   
-  validates :part_id, :qty, :inventory_list_id, presence: true
+  validates :qty, :inventory_list_id, presence: true
   validates_inclusion_of :in_store, in: [true, false]
   
   def self.new_item(package_id, unique_id, part_id, qty, position, inventory_list_id, user_id)
@@ -13,6 +13,9 @@ class InventoryListItem < ActiveRecord::Base
     
     # 根据参数组合情况获取nstorage start
     if !package_id.blank?
+      if InventoryListItem.where(package_id: package_id).first
+        return nil
+      end
       query = NStorage.where("packageid = :packageid ",{:packageid => package_id})
     elsif !unique_id.blank?
       query = NStorage.where("uniqueid = :uniqueid ",{:uniqueid => unique_id})
@@ -26,7 +29,7 @@ class InventoryListItem < ActiveRecord::Base
     end
     # 根据参数组合情况获取nstorage end
     
-    if query.blank? 
+    if query.blank?
       # 未入库，直接生成
       current_whouse = nil
       current_position = nil
@@ -54,17 +57,18 @@ class InventoryListItem < ActiveRecord::Base
         part_id = query[0].partNr
       end
     end
-    
+
     # 赋值
     inventory_list_item = InventoryListItem.new(:package_id => package_id,:unique_id => unique_id,
     :part_id => part_id,:qty => qty,:position => position,:current_whouse => current_whouse,
     :current_position => current_position,:user_id => user_id,:in_store => in_store,
     :inventory_list_id => inventory_list_id)
-    
+    puts inventory_list_item.to_json
     # 保存
-    if inventory_list_item.save
+    if inventory_list_item.save!
       return inventory_list_item
     else
+
       return nil
     end
     
