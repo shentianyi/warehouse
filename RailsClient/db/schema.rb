56012599b847a196f7fecb58b63a9689b8b5a2f7
Charlot <7wangsong@gmail.com>
2015-06-25 14:29:34 +0800

    update column type

--- RailsClient/db/schema.rb
+++ RailsClient/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625025418) do
+ActiveRecord::Schema.define(version: 20150625032200) do
 
   create_table "api_logs", force: true do |t|
     t.string   "user_id"
@@ -293,7 +293,7 @@ ActiveRecord::Schema.define(version: 20150625025418) do
     t.integer  "type_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "qty",          precision: 9, scale: 2
+    t.decimal  "qty",          precision: 20, scale: 10
   end
 
   add_index "movements", ["packageId"], name: "package_id_index", using: :btree
@@ -323,7 +323,7 @@ ActiveRecord::Schema.define(version: 20150625025418) do
     t.string   "ware_house_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "qty",           precision: 9, scale: 2
+    t.decimal  "qty",           precision: 20, scale: 10
   end
 
   add_index "n_storages", ["packageId"], name: "package_id_index", using: :btree
@@ -455,17 +455,17 @@ ActiveRecord::Schema.define(version: 20150625025418) do
   add_index "part_types", ["id"], name: "index_part_types_on_id", using: :btree
 
   create_table "parts", force: true do |t|
-    t.string   "uuid",         limit: 36,                                          null: false
+    t.string   "uuid",         limit: 36,                                           null: false
     t.string   "customernum"
     t.string   "user_id"
-    t.boolean  "is_delete",                                        default: false
-    t.boolean  "is_dirty",                                         default: true
-    t.boolean  "is_new",                                           default: true
+    t.boolean  "is_delete",                                         default: false
+    t.boolean  "is_dirty",                                          default: true
+    t.boolean  "is_new",                                            default: true
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "unit_pack"
     t.string   "part_type_id"
-    t.decimal  "convert_unit",            precision: 10, scale: 0, default: 1
+    t.decimal  "convert_unit",            precision: 20, scale: 10, default: 1.0
   end
 
   add_index "parts", ["id"], name: "index_parts_on_id", using: :btree
