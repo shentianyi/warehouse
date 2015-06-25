21f02971a39fdfff2ae6bd20a8aae1bde032908f
Charlot <7wangsong@gmail.com>
2015-06-26 07:16:18 +0800

    disable enterstock

--- RailsClient/script/migrate/0625check_stock/03restore_packages.rb
+++ RailsClient/script/migrate/0625check_stock/03restore_packages.rb
@@ -3,7 +3,7 @@ NStorage.transaction do
   Movement.where("packageId like 'WI%'").delete_all
 
   LogisticsContainer.uniq.joins(:package).each do |lc|
-    puts
+    puts lc.to_json
     lc.enter_stock
   end
 end
