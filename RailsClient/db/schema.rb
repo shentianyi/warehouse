195d5a780cfec45dfff80f21ed3035faadab9184
Charlot <7wangsong@gmail.com>
2015-06-25 14:30:58 +0800

    schema

--- RailsClient/db/schema.rb
+++ RailsClient/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625025418) do
+ActiveRecord::Schema.define(version: 20150624032200) do
 
   create_table "api_logs", force: true do |t|
     t.string   "user_id"
@@ -455,17 +455,16 @@ ActiveRecord::Schema.define(version: 20150625025418) do
   add_index "part_types", ["id"], name: "index_part_types_on_id", using: :btree
 
   create_table "parts", force: true do |t|
-    t.string   "uuid",         limit: 36,                                          null: false
+    t.string   "uuid",         limit: 36,                 null: false
     t.string   "customernum"
     t.string   "user_id"
-    t.boolean  "is_delete",                                        default: false
-    t.boolean  "is_dirty",                                         default: true
-    t.boolean  "is_new",                                           default: true
+    t.boolean  "is_delete",               default: false
+    t.boolean  "is_dirty",                default: true
+    t.boolean  "is_new",                  default: true
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "unit_pack"
     t.string   "part_type_id"
-    t.decimal  "convert_unit",            precision: 10, scale: 0, default: 1
   end
 
   add_index "parts", ["id"], name: "index_parts_on_id", using: :btree
