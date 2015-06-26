4f83cb1515ac87311e7f851177fd0ec024984da7
Charlot <7wangsong@gmail.com>
2015-06-26 09:42:03 +0800

    fi

--- RailsClient/script/migrate/0625check_stock/04fix_inventory_fifo.rb
+++ RailsClient/script/migrate/0625check_stock/04fix_inventory_fifo.rb
@@ -2,7 +2,7 @@ InventoryListItem.transaction do
   InventoryListItem.where(fifo: nil).where("package_id<>''").each do |item|
     puts item.to_json
     if storage=NStorage.where(packageId: item.package_id).first
-      puts "##{item.package_idgita }"
+      puts "##{item.package_id}"
       item.update_attributes(fifo: storage.fifo)
     end
   end
