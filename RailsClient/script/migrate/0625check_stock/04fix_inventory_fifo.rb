ddc84371e4c1d18809789c90f07a0ef3689ec5aa
Charlot <7wangsong@gmail.com>
2015-06-26 09:40:55 +0800

    fix fifo

---
+++ RailsClient/script/migrate/0625check_stock/04fix_inventory_fifo.rb
@@ -0,0 +1,9 @@
+InventoryListItem.transaction do
+  InventoryListItem.where(fifo: nil).where("package_id<>''").each do |item|
+    puts item.to_json
+    if storage=NStorage.where(packageId: item.package_id).first
+      puts "##{item.package_idgita }"
+      item.update_attributes(fifo: storage.fifo)
+    end
+  end
+end
