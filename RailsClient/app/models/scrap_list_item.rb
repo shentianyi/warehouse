9093ba3731c497f7521b2c449424fb9b8b9e4440
Charlot <7wangsong@gmail.com>
2015-06-30 10:59:06 +0800

    update

--- RailsClient/app/models/scrap_list_item.rb
+++ RailsClient/app/models/scrap_list_item.rb
@@ -2,13 +2,14 @@ class ScrapListItem < ActiveRecord::Base
   belongs_to :scrap_list
 
   def scrap
+
     params={
         partNr: self.part_id,
         qty: self.quantity,
-        toWh: 'BaofeiKu',
-        toPosition:'BaofeiWeizhi',
-        fromWh:'3EX'
+        toWh: self.scrap_list.dse_warehouse,
+        fromWh:self.scrap_list.src_warehouse
     }
+
     WhouseService.new.move(params)
     self.update_attributes(state:ScrapListItemState::HANDLED)
   end
