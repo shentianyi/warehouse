3b28dcadea23386c9ad008d263c80680d3a86fc8
lzding <472339105@qq.com>
2015-07-14 12:01:45 +0800

    .

--- RailsClient/db/schema.rb
+++ RailsClient/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629025042) do
+ActiveRecord::Schema.define(version: 20150706063549) do
 
   create_table "api_logs", force: true do |t|
     t.string   "user_id"
@@ -292,6 +292,7 @@ ActiveRecord::Schema.define(version: 20150629025042) do
   create_table "movements", force: true do |t|
     t.string   "partNr"
     t.datetime "fifo"
+    t.decimal  "qty",          precision: 20, scale: 10
     t.string   "from_id"
     t.string   "fromPosition"
     t.string   "to_id"
@@ -301,10 +302,9 @@ ActiveRecord::Schema.define(version: 20150629025042) do
     t.integer  "type_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "qty",          precision: 20, scale: 10
+    t.string   "remark"
     t.string   "remarks"
     t.string   "employee_id"
-    t.string   "remark"
   end
 
   add_index "movements", ["packageId"], name: "package_id_index", using: :btree
@@ -328,13 +328,13 @@ ActiveRecord::Schema.define(version: 20150629025042) do
     t.string   "storageId"
     t.string   "partNr"
     t.datetime "fifo"
+    t.decimal  "qty",           precision: 20, scale: 10
     t.string   "position"
     t.string   "packageId"
     t.string   "uniqueId"
     t.string   "ware_house_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "qty",           precision: 20, scale: 10
     t.boolean  "locked",                                  default: false
   end
 
@@ -735,6 +735,17 @@ ActiveRecord::Schema.define(version: 20150629025042) do
   add_index "users", ["location_id"], name: "index_users_on_location_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uuid"], name: "index_users_on_uuid", using: :btree
+
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
   create_table "ware_houses", force: true do |t|
     t.string   "whId"
