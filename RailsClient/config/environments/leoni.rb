ebeb2a8a5302ae3534f8d5f567eeea213b1dc3fb
zerosong <evil_song@126.com>
2015-07-30 20:49:46 +0800

    asset

--- RailsClient/config/environments/leoni.rb
+++ RailsClient/config/environments/leoni.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.allow_concurrency = false
-
+  config.assets.compile = false
   # Do not eager load code on boot.
   config.eager_load = false
 
