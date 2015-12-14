6ad5b94f8239b0b93d8903a988df26f50ad44b2e
lzding <472339105@qq.com>
2015-12-14 12:09:42 +0800

    fix search movement source

--- RailsClient/app/controllers/movement_sources_controller.rb
+++ RailsClient/app/controllers/movement_sources_controller.rb
@@ -3,8 +3,52 @@ class MovementSourcesController < ApplicationController
 
   respond_to :html
 
+  def search
+    @condition=params[@model]
+    query=MovementSource.joins(:movement_list)
+              .select("movement_sources.*, movement_lists.state as state")
+              .where("movement_lists.state != #{MovementListState::ENDING}")
+    @condition.each do |k, v|
+      if (v.is_a?(Fixnum) || v.is_a?(String)) && !v.blank?
+        puts @condition.has_key?(k+'_fuzzy')
+        if @condition.has_key?(k+'_fuzzy')
+          query=query.where("#{k} like ?", "%#{v}%")
+        else
+          query=query.where(Hash[k, v])
+        end
+        instance_variable_set("@#{k}", v)
+      end
+      #if v.is_a?(Array) && !v.empty?
+      #  query= v.size==1 ? query.where(Hash[k, v[0]]) : query.in(Hash[k, v]
+      #end
+      #query=query.where(Hash[k, v]) if v.is_a?(Range)
+      if v.is_a?(Hash) && v.values.count==2 && v.values.uniq!=['']
+        values=v.values.sort
+        values[0]=Time.parse(values[0]).utc.to_s if values[0].is_date? & values[0].include?('-')
+        values[1]=Time.parse(values[1]).utc.to_s if values[1].is_date? & values[1].include?('-')
+        query=query.where(Hash[k, (values[0]..values[1])])
+        v.each do |kk, vv|
+          instance_variable_set("@#{k}_#{kk}", vv)
+        end
+      end
+    end
+
+    if params.has_key? "download"
+      send_data(query.to_xlsx(query),
+                :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet",
+                :filename => @model.pluralize+".xlsx")
+      #render :json => query.to_xlsx(query)
+    else
+      instance_variable_set("@#{@model.pluralize}", query.paginate(:page => params[:page]).all)
+      render :index
+    end
+  end
+
   def index
-    @movement_sources = MovementSource.all.paginate(:page => params[:page])
+    @movement_sources = MovementSource.joins(:movement_list)
+                            .select("movement_sources.*, movement_lists.state as state")
+                            .where("movement_lists.state != #{MovementListState::ENDING}").paginate(:page => params[:page])
+    # @movement_sources = MovementSource.all.paginate(:page => params[:page])
     respond_with(@movement_sources)
   end
 
