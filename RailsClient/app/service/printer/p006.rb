af43a67a138c76e96408e19f36329224908c6a26
Charlot <7wangsong@gmail.com>
2016-03-20 22:46:07 +0800

    fix

--- RailsClient/app/service/printer/p006.rb
+++ RailsClient/app/service/printer/p006.rb
@@ -1,29 +1,36 @@
 # print pick list
 module Printer
   class P006<Base
-    HEAD=[:pl_nr, :user_id, :created_at]
-    BODY=[:part_id, :quantity, :box_quantity,:whouse_id, :is_emergency, :remark]
+    HEAD=[:pl_nr, :created_date]
+    BODY=[:leoni_nr, :czleoni_nr, :ask_qty, :position, :kucun, :send_nr, :remark]
 
     def generate_data
       p=PickList.find_by_id(self.id)
-      head={id: p.id, user_id: p.user_id, created_at: p.created_at.localtime.strftime('%Y.%m.%d %H:%M:%S')}
+      head={
+          pl_nr: p.id,
+          created_date: p.created_at.localtime.strftime('%Y.%m.%d %H:%M:%S')
+      }
       heads=[]
       HEAD.each do |k|
         heads<<{Key: k, Value: head[k]}
       end
 
-      pick_items=p.pick_items.where(state:PickItemState::PRINTING).order(state: :asc, is_emergency: :desc)
+      # pick_items=p.pick_items.where(state: PickItemState::PRINTING).order(state: :asc, is_emergency: :desc)
+      pick_items=p.pick_items.select("SUM(pick_items.quantity) as total, pick_items.*").group("part_id").order(state: :asc, is_emergency: :desc)
+
       if pick_items.count == 0
         pick_items = p.pick_items.order(state: :asc, is_emergency: :desc)
       end
 
       pick_items.each do |i|
+
         body= {
-            part_id: i.part_id,
-            quantity: i.quantity,
-            box_quantity: i.box_quantity,
-            whouse_id:i.destination_whouse_id,
-            is_emergency: i.is_emergency ? '是' : ' ',
+            leoni_nr: i.part_id,
+            czleoni_nr: i.part_id,
+            ask_qty: i.quantity,
+            position: i.box_quantity,
+            kucun: i.destination_whouse_id,
+            send_nr: i.quantity,
             remark: i.remark||' '
         }
 
@@ -32,7 +39,7 @@ module Printer
           bodies<<{Key: k, Value: body[k]}
         end
         self.data_set <<(heads+bodies)
-        i.update(state:PickItemState::PRINTED)
+        i.update(state: PickItemState::PRINTED)
       end
     end
   end
