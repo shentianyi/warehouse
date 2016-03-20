eb8c97d19481a1c8162a9b8a4e35d4b21cf35222
Charlot <7wangsong@gmail.com>
2016-03-20 17:32:55 +0800

    short

--- RailsClient/app/models/forklift.rb
+++ RailsClient/app/models/forklift.rb
@@ -2,7 +2,7 @@ class Forklift<Container
   default_scope { where(type: ContainerType::FORKLIFT) }
 
   def generate_id
-    "F#{Time.now.strftime('%Y%m%d%H%M%S')}"
+    "F#{Time.now.strftime('%y%m%d%H%M%S')}"
   end
 
 end
