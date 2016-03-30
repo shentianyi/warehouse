1a93e0984f6a8015c74c2e3ed32cd7eecff1a610
Charlot <7wangsong@gmail.com>
2016-03-30 09:23:36 +0800

    fix

--- RailsClient/script/migrate/balance_storage.rb
+++ RailsClient/script/migrate/balance_storage.rb
@@ -11,10 +11,10 @@ condition[:created_at]=Time.parse('2015-12-12 00:00:00').utc.to_s...Time.parse('
 count=0
 NStorage.transaction do
   storages=NStorage.where(ware_house_id: 'SR01')
-               .select("SUM(n_storages.qty) as total_qty, n_storages.qty as qty, n_storages.partNr as partNr, n_storages.ware_house_id as ware_house_id, n_storages.position as position")
+               .select("SUM(n_storages.qty) as total_qty, n_storages.*")
                .group(:partNr, :ware_house_id, :position)
-  # storages=NStorage.where(ware_house_id: 'SR01', partNr: '76755104W000', position: 'SR01')
-  #              .select("SUM(n_storages.qty) as total_qty, n_storages.qty as qty, n_storages.partNr as partNr, n_storages.ware_house_id as ware_house_id, n_storages.position as position")
+  # storages=NStorage.where(ware_house_id: 'SR01', position: 'SR01', partNr: '76755104W000')
+  #              .select("SUM(n_storages.qty) as total_qty, n_storages.*")
   #              .group(:partNr, :ware_house_id, :position)
   storages.each do |storage|
     count+=1
@@ -62,12 +62,14 @@ NStorage.transaction do
             .select("SUM(movements.qty) as move_out_qty").first.move_out_qty.to_f
 
     end
-    # puts "#{b}--------#{c}------------#{d}-------------#{storage.qty.to_f}-------"
+    puts "- #{b}-------- #{c} ------------ #{d} -------------(#{storage.qty.to_f})-------"
 
     calc_storage_qty=b + c - d
     unless storage.total_qty==calc_storage_qty
       qty=storage.qty.to_f-storage.total_qty.to_f+calc_storage_qty
-      storage.update_attribute(:qty, qty)
+      # puts "update #{qty}"
+      storage.update_attributes(qty: qty)
+      # p storage
     end
   end
 end
