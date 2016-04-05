8c880b9d6aa1ae5346d4bc7e80ca640a6e435e88
Songfuzhen <evil_song@126.com>
2016-04-05 09:43:24 +0800

    mobile login

--- RailsClient/config/application.rb
+++ RailsClient/config/application.rb
@@ -10,7 +10,7 @@ module Warehouse
   class Application < Rails::Application
     #config.paths['config/database']='config/wangsong_database.yml' if ENV['USER']=='wangsong'
     #config.paths['config/database']='config/charlot_database.yml' if ENV['USER']=='charlot'
-   # config.paths['config/database']='config/mac_database.yml' if ENV['USER']=='liqi'
+   config.paths['config/database']='config/mac_database.yml' if ENV['USER']=='zero'
     config.paths['config/database']='config/leoni_root_database.yml' if ENV['USER']=='root'
     config.paths['config/database']='config/mac_database.yml' if /darwin\w+/.match(RbConfig::CONFIG['host_os'])
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
