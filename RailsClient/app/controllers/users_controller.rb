5741dd066e87cd7782ec8b1cea05fdf762af61b9
lzding <lzding@users.noreply.github.com>
2016-04-21 11:17:39 +0800

    Update users_controller.rb

--- RailsClient/app/controllers/users_controller.rb
+++ RailsClient/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate(:page => params[:page])
+    @users = User.order(nr: :desc).paginate(:page => params[:page])
     #@users = @users.paginate(:page=>params[:page])
   end
 
