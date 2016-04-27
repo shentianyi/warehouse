39c4de858577bd7f8a08e8f810f442a9d8c050a3
HuHang <m18954237672_1@163.com>
2016-04-27 15:17:22 +0800

    add warehouse_id position_id

--- RailsClient/app/api/v1/package_api.rb
+++ RailsClient/app/api/v1/package_api.rb
@@ -39,6 +39,9 @@ module V1
         args[:part_id] = "P#{storage.partNr}"
         args[:qty] = storage.qty
         args[:fifo] = storage.fifo.blank? ? '' : storage.fifo.localtime
+
+        args[:fromWh] = storage.ware_house_id
+        args[:fromPosition] = storage.position_id
         {result: '1', content: args}
       end
 
