b7b72270dd43451bd39589b827c8c9f9090774c9
lzding <472339105@qq.com>
2016-09-01 14:25:17 +0800

    add record export

--- RailsClient/app/models/storage_operation_record.rb
+++ RailsClient/app/models/storage_operation_record.rb
@@ -1,4 +1,5 @@
 class StorageOperationRecord < ActiveRecord::Base
+  belongs_to :move_type
 
   def self.save_record(params, type)
     record = {
@@ -11,11 +12,43 @@ class StorageOperationRecord < ActiveRecord::Base
         fromPosition: (params[:fromPosition] if params[:fromPosition].present?),
         fifo: (params[:fifo] if params[:fifo].present?),
         partNr: (params[:partNr] if params[:partNr].present?),
-        packageId:(params[:packageId] if params[:packageId].present?),
+        packageId: (params[:packageId] if params[:packageId].present?),
         qty: (params[:qty] if params[:qty].present?)
     }
 
     puts record
     StorageOperationRecord.create(record)
+  end
+
+
+  def self.to_xlsx records
+    p = Axlsx::Package.new
+
+    wb = p.workbook
+    wb.add_worksheet(:name => "sheet1") do |sheet|
+      sheet.add_row ["序号", "零件号", "唯一码", "源仓库", "源库位", "目的仓库", "目的库位", "数量", "FIFO", "类型", "备注", "员工号", "创建时间"]
+      records.each_with_index { |record, index|
+
+
+
+        sheet.add_row [
+                          index+1,
+                          record.partNr,
+                          record.packageId,
+                          record.fromWh,
+                          record.fromPosition,
+                          record.toWh,
+                          record.toPosition,
+
+                          record.qty,
+                          record.fifo,
+                          record.move_type.blank? ? '' : record.move_type.typeId,
+                          record.remark,
+                          record.employee_id,
+                          record.created_at.present? ? record.created_at.localtime.strftime("%Y-%m-%d %H:%M") : ''
+                      ]
+      }
+    end
+    p.to_stream.read
   end
 end
