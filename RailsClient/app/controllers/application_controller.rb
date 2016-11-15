e3de80ce4c5759a1d2ff9d166f8a5016b6a2a3a8
lzding <472339105@qq.com>
2016-11-15 09:41:40 +0800

    fix

--- RailsClient/app/controllers/application_controller.rb
+++ RailsClient/app/controllers/application_controller.rb
@@ -63,13 +63,13 @@ class ApplicationController < ActionController::Base
 
   private
   def authenticate_user_from_token!
-    # Need to pass email and token every request
-    user_email = params[:user_email].presence
-    user = user_email && User.find_by_email(user_email)
-
-    if user && Devise.secure_compare(user.authentication_token, params[:user_token])
-      sign_in user, store: false
-    end
+    # # Need to pass email and token every request
+    # user_email = params[:user_email].presence
+    # user = user_email && User.find_by_email(user_email)
+    #
+    # if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+    #   sign_in user, store: false
+    # end
   end
 
 
--- RailsClient/app/controllers/users/sessions_controller.rb
+++ RailsClient/app/controllers/users/sessions_controller.rb
@@ -6,6 +6,8 @@ class Users::SessionsController<Devise::SessionsController
   end
 
   def create
+    raise 'ssssssssssssss'
+
     self.resource = warden.authenticate!(auth_options)
     #set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
