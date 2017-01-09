84c1a8ad4654dddd9f3d580f628867e717b22f60
lzding <472339105@qq.com>
2017-01-09 20:52:10 +0800

    fix

--- RailsClient/app/models/led.rb
+++ RailsClient/app/models/led.rb
@@ -1,16 +1,20 @@
 class Led < ActiveRecord::Base
-  include Extensions::UUID
+  # include Extensions::UUID
   include Import::LedCsv
   belongs_to :modem
   belongs_to :position
   belongs_to :order_box, class_name: 'OrderBox'
   belongs_to :order_car, class_name: 'OrderCar'
 
-  validates_uniqueness_of :id
+  validates_uniqueness_of :id, :scope => :modem_id if bind_led?
   validate :validate_save
 
 
   before_create :init_led_display
+
+  def self.bind_led?
+    self.modem_id.blank? ? false : true
+  end
 
   def init_led_display
     self.led_display = self.led_display.nil? ? '0000' : self.led_display
