55b49514ed463da4d047a3245084c82c00a492f0
Charlot <7wangsong@gmail.com>
2017-01-11 11:03:53 +0800

    fix

--- RailsClient/config/application.rb
+++ RailsClient/config/application.rb
@@ -13,6 +13,7 @@ module Warehouse
     config.paths['config/database']='config/mac_database.yml' if ENV['USER']=='zero'
     config.paths['config/database']='config/leoni_root_database.yml' if ENV['USER']=='root'
     config.paths['config/database']='config/mac_database.yml' if /darwin\w+/.match(RbConfig::CONFIG['host_os'])
+    config.paths['config/database']='config/database.yml'
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 #raise ENV['USER'] 
 # for the api
